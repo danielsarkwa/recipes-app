b987df358c0eddbbf410a6b08aa539f812667848
Dhanny <dannysark16@gmail.com>
2019-12-28 11:33:45 +0000

    prepared data for posting the signup to api

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
+  signup(email: string, password: string) {
+    this.http.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAeh4QXJCSigVYadkAKWcBFsZkAMBFO4EY',
+    {
+      email: email,
+      password: password,
+      returnSecuredToken: true
+    });
+  }
 }
